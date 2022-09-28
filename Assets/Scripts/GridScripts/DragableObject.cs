6811c1b7e252ee70b50ab2bc571e95de803f2ced
Antonio Wall <antoniowallferrazneto@yahoo.com.br>
2022-09-28 02:27:59 -0300

    Sistema de Grid Implementado
    demorou mas chegou

---
+++ Assets/Scripts/GridScripts/DragableObject.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class DragableObject : MonoBehaviour
+{
+    [SerializeField]private GridCell currentCell = null, previousCell = null;
+    private GridCell CheckForCell() //Método que retorna a célula
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition); //Cria um raio a partir da posição do mouse do player
+        if(Physics.Raycast(mouseRay, out RaycastHit raycastHit) == true) //se o raycast criado pelo mouse atinge alguma coisa
+        {
+            return raycastHit.transform.gameObject.GetComponent<GridCell>(); //retorna o gridcell atingido pelo raycast
+        }
+        else
+        {
+            return null; //retorna nada se não atingir nada
+        }
+    }
+
+    private void OnMouseDrag() //é chamado quando movo o mouse
+    {
+        if (MouseRaycast.MousePos() != Vector3.zero) //o mouse está atingindo uma célula do grid?
+        {
+            transform.position = MouseRaycast.MousePos(); //snap
+            if(CheckForCell() != null) //checa a existência de uma célula
+            {
+                previousCell = currentCell; //cicla célula nova com a anterior
+                currentCell = CheckForCell(); //pega a célula 
+                if(previousCell != null) //exceção da existência de uma célula anterior, e esvazia a célula anterior
+                {
+                    previousCell.currentObject = null;
+                    previousCell.isOccupied = false;
+                }
+                
+                if(currentCell.isOccupied == false) //Checa se a célula já está ocupada
+                {
+                    currentCell.currentObject = gameObject; //ocupa a célula nova
+                    currentCell.isOccupied = true;
+                }
+            }
+        }
+    }
+
+    public void Rotate()
+    {
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+    }
+}
---
+++ Assets/Scripts/GridScripts/GridSystem.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    [SerializeField]private float sizeX, sizeZ;
+    [SerializeField]private Vector3 gridHead;
+    [SerializeField]private GameObject grid, gridCell;
+    public static GridCell currentSelectedCell;
+    private bool isGridEnabled;
+    
+    private void Start()
+    {
+        InitGrid(); //Carrega todos as células de grid
+    }
+
+    private void InitGrid()
+    {
+        Vector3 pos = gridHead; //posição na qual a próxima grid será iniciada
+        for (int i = 0; i < sizeZ; i++, pos.z--) //for para percorrer todo o grid
+        {
+            pos.x = gridHead.x; //reseta a posição em x, necessário para não fazer uma escada de grids
+            for (int j = 0; j < sizeX; j++, pos.x++)
+            {
+                GameObject currentCell = Instantiate(gridCell, pos, transform.rotation); //instancia a célula de grid
+                GridCell cellData = currentCell.GetComponent<GridCell>(); //acessa os componentes da célula
+                currentCell.transform.SetParent(grid.transform); //seta o grid como filho do objeto de grid
+                cellData.tilePos = $"{i+1}x{j+1}"; //atribuí na célula a sua posição
+            }
+        }
+    }
+
+    public void GridToggle(bool toggle) //ativa ou desativa a visibilidade do grid;
+    {
+        grid.SetActive(toggle);
+    }
+}
