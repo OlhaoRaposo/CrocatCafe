f2c22ea8c7371e6b56b1ba44b5ec8e9eabbe14fd
Daniel Domingos de Oliveira Souza <danieldomingosdev@gmail.com>
2022-09-28 16:26:10 -0300

    +NavMesh

---
+++ Assets/Scripts/NavMeshScript.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshScript : MonoBehaviour
+{
+    public GameObject destino;
+    public GameObject chillPlace;
+    public NavMeshAgent navMesh;
+    public int bakeTime;
+
+     void Start()
+    {
+    }
+
+    void Update()
+    {
+        //Substituir por um Getcomponent no gatinho com o bakeDestin e o objeto onde ele vai andar
+        if (Input.GetKey(KeyCode.E)) {
+            BakeDestin(destino);
+        }
+    }
+    public void BakeDestin(GameObject obj)
+    {
+        navMesh.destination = obj.transform.position;
+        StartCoroutine(CheckDestination(obj));
+    }
+
+    IEnumerator AfterBakeDestin()
+    {
+        yield return new WaitForSeconds(bakeTime);
+        navMesh.destination = chillPlace.transform.position;
+    }
+
+    IEnumerator CheckDestination(GameObject obj)
+    {
+        yield return new WaitForSeconds(1);
+        if (transform.position.magnitude - obj.transform.position.magnitude >= .5f)
+        {
+            StartCoroutine(AfterBakeDestin());
+            StopCoroutine(CheckDestination(obj));
+        }
+        else
+            StartCoroutine(CheckDestination(obj));
+        Debug.Log(transform.position.magnitude - obj.transform.position.magnitude);
+        Debug.Log("Check");
+    }
+}
