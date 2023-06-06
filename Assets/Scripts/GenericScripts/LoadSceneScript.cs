0d53b6a1451feb25609298f10745da5bc822ff58
Antonio Wall <antoniowallferrazneto@yahoo.com.br>
2023-06-05 22:48:27 -0300

    Limpeza do TI

---
+++ Assets/Scripts/GenericScripts/LoadSceneScript.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    //Pega uma string como referencia pra dar Load
+    //essa string e passada no proprio botao que irá carregar a cena.
+    public void LoadScene(string sceneName)
+    {
+        if (SceneManager.GetActiveScene().name != sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+            return;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/GenericScripts/ProgressBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Slider progressBar;
+    [SerializeField] private float loadTime;
+
+    public void FixedUpdate()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+    }
+
+    public void StartLoading(float loadTime)
+    {
+        this.loadTime = loadTime;
+        InvokeRepeating("UpdateProgress", 0, 1);
+    }
+
+    private void UpdateProgress()
+    {
+        if (progressBar.value < 1.0f)
+        {
+            progressBar.value += 1 / loadTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
